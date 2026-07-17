98c4b4565ff57c7617931605213a9f19f21b9e82
yanit sharma <yanit.sharma@unthinkable.co>
2026-07-17 11:03:32 +0530

    firnl

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -6,7 +6,6 @@ using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi;
 using Practice;
 using Practice.Configurations;
